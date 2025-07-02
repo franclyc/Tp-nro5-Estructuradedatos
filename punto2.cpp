308407d3dac3bd24f92cb42627f9ea3ae766212e
franclyc <francoguanactolay61@gmail.com>
2025-07-01 22:00:15 -0300

    p2

--- punto2.cpp
+++ punto2.cpp
@@ -4,7 +4,7 @@ struct Nodo {
     int dato;
     Nodo* siguiente;
 };
-struct lista {
+struct Lista {
     Nodo* inicio;
     Nodo* final;
     int cantidad;
@@ -37,4 +37,50 @@ int main(){
 
     
 }
+void iniciar_lista(Lista &lista) {
+    lista.inicio = nullptr;
+    lista.final = nullptr;
+    lista.cantidad = 0;
+}
+void agregar_final(Lista &lista, int valor) {
+    Nodo* nuevo = new Nodo{valor, nullptr};
+    if (lista.inicio == nullptr) {
+        lista.inicio = nuevo;
+        lista.final = nuevo;
+    } else {
+        lista.final->siguiente = nuevo;
+        lista.final = nuevo;
+    }
+    lista.cantidad++;
+}   
 
+Nodo* eliminar_inicio(Lista &lista) {
+        Nodo* borrado;
+
+    if (lista.inicio == nullptr) {
+        borrado = nullptr;
+    } else {
+        borrado = lista.inicio;
+        lista.inicio = borrado->siguiente;
+        borrado->siguiente = nullptr;
+
+        if (lista.inicio == nullptr) {
+            lista.final = nullptr;
+        }
+
+        lista.cantidad--;
+    }
+    return borrado;
+}
+
+void mostrar_lista(Lista lista) {
+    Nodo* aux = lista.inicio;
+    while (aux != nullptr) {
+        cout << aux->dato << " -> ";
+        aux = aux->siguiente;
+    }
+    cout << "null" << endl;
+}
+int obtener_cantidad(Lista lista) {
+    return lista.cantidad;
+}
