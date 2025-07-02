c729a91d2c145f960b0c11d5c5b2b0810275027f
franclyc <francoguanactolay61@gmail.com>
2025-07-02 01:54:22 -0300

    primer codigo del p7
    imprime los numeros alreves

---
+++ punto7.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+class Nodo {
+private:
+    int dato;
+    Nodo* sig;
+public:
+    Nodo(int dato) {
+        this->dato = dato;
+        this->sig = nullptr;
+    }
+
+    int getDato() {
+        return dato;
+    }
+
+    Nodo* getSiguiente() {
+        return sig;
+    }
+
+    void setSiguiente(Nodo* nuevo) {
+        sig = nuevo;
+    }
+};
+
+class Lista {
+private:
+    Nodo* inicio;
+    int cant;
+public:
+    Lista() {
+        inicio = nullptr;
+        cant = 0;
+    }
+
+    bool estaVacia() {
+        return inicio == nullptr;
+    }
+
+    void AgregarOrdenado(Nodo* nuevo) {
+        if (estaVacia()) {
+            inicio = nuevo;
+        }
+        else {
+            if (nuevo->getDato() < inicio->getDato()) {
+                nuevo->setSiguiente(inicio);
+                inicio = nuevo;
+            }
+            else {
+                Nodo* i = inicio;
+                while (i->getSiguiente() != nullptr && i->getSiguiente()->getDato() < nuevo->getDato()) {
+                    i = i->getSiguiente();
+                }
+                nuevo->setSiguiente(i->getSiguiente());
+                i->setSiguiente(nuevo);
+            }
+        }
+        cant++;
+    }
+
+    void MostrarLista() {
+        if (estaVacia()) {
+            cout << "Lista vacía" << endl;
+        }
+        else {
+            Nodo* i = inicio;
+            while (i != nullptr) {
+                cout << i->getDato() << " ";
+                i = i->getSiguiente();
+            }
+            cout << endl;
+        }
+    }
+
+    Nodo* getInicio() {
+        return inicio;
+    }
+};
+
+// ✅ Función enigma (del punto 7)
+void enigma(Nodo* nodo) {
+    if (nodo != nullptr) {
+        if (nodo->getSiguiente() == nullptr)
+            cout << nodo->getDato() << endl;
+        else {
+            enigma(nodo->getSiguiente());
+            cout << nodo->getDato() << endl;
+        }
+    }
+}
+
+int main() {
+    Lista lista;
+    lista.AgregarOrdenado(new Nodo(6));
+    lista.AgregarOrdenado(new Nodo(4));
+    lista.AgregarOrdenado(new Nodo(9));
+    lista.AgregarOrdenado(new Nodo(5));
+    lista.AgregarOrdenado(new Nodo(8));
+
+    cout << "Lista normal (ordenada):" << endl;
+    lista.MostrarLista();
+
+    cout << "\n orden inverso:" << endl;
+    enigma(lista.getInicio());
+
+    return 0;
+}
