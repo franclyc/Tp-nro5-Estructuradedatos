539f543f14363c7fe505b9971f79496dfb6aec0e
franclyc <francoguanactolay61@gmail.com>
2025-07-02 01:41:14 -0300

    menu y funciones del punto 6b

--- punto 6b.cpp
+++ punto 6b.cpp
@@ -69,3 +69,69 @@ public:
         return inicio;
     }
 };
+
+
+void contarMayusMinus(ListaChar& lista, int& mayus, int& minus) {
+    mayus = minus = 0;
+    NodoChar* actual = lista.getInicio();
+    while (actual != nullptr) {
+        char c = actual->getDato();
+        if (isupper(c)) {
+            mayus++;
+        } else if (islower(c)) {
+            minus++;
+        }
+        actual = actual->getSiguiente();
+    }
+}
+
+
+void menu() {
+    ListaChar lista;
+    int opcion;
+    char caracter;
+
+    do {
+        cout << "\n--- MENU ---\n";
+        cout << "1. Agregar caracter a la lista\n";
+        cout << "2. Mostrar lista\n";
+        cout << "3. Contar mayusculas y minusculas\n";
+        cout << "0. Salir\n";
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese un caracter: ";
+                cin >> caracter;
+                lista.AgregarFinal(new NodoChar(caracter));
+                cout << "Caracter agregado.\n";
+                break;
+
+            case 2:
+                lista.MostrarLista();
+                break;
+
+            case 3: {
+                int mayus, minus;
+                contarMayusMinus(lista, mayus, minus);
+                cout << "Cantidad de mayusculas: " << mayus << endl;
+                cout << "Cantidad de minusculas: " << minus << endl;
+                break;
+            }
+
+            case 0:
+                cout << "Saliendo del programa...\n";
+                break;
+
+            default:
+                cout << "Opcion invalida. Intente de nuevo.\n";
+        }
+
+    } while (opcion != 0);
+}
+
+int main() {
+    menu();
+    return 0;
+}
