ed69d77012c177fb9acd5abd620865e4f0f9af47
franclyc <francoguanactolay61@gmail.com>
2025-07-02 23:55:12 -0300

    punto 2
    agregar el quit inicial y quit final

---
+++ tp6/punto2.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+typedef struct tnodo *pnodo;
+typedef struct tnodo{
+                    int dato;
+                    pnodo sig;
+                    pnodo ant;
+                    };
+typedef struct tlista{
+                    pnodo inicio;
+                    pnodo fin;
+                    };
+
+void IniciarLista(tlista &lista)
+{
+    lista.inicio = NULL;
+    lista.fin = NULL;
+}
+
+void CrearNodo(pnodo &nuevo, int valor)
+{
+    nuevo = new tnodo;
+    if(nuevo != NULL)
+    {
+        nuevo -> dato = valor;
+        nuevo -> sig = NULL;
+        nuevo -> ant = NULL;
+    }
+}
+
+void AgregarInicio(tlista &lista, pnodo nuevo)
+{
+    if(lista.inicio == NULL){
+        lista.inicio = nuevo;
+        lista.fin = nuevo;
+    }
+    else{
+        nuevo->sig = lista.inicio;
+        lista.inicio->ant = nuevo;
+        lista.inicio = nuevo;
+    }
+}
+
+void AgregarFinal(tlista &lista, pnodo nuevo){
+    if(lista.inicio == NULL){
+        lista.inicio = nuevo;
+        lista.fin = nuevo;
+    }
+    else{
+        lista.fin->sig = nuevo;
+        nuevo->ant = lista.fin;
+        lista.fin = nuevo;
+    }
+}
+
+void QuitarInicio(){
+    //Completar el codigo
+}
+
+void QuitarFinal(){
+    //Completar el codigo
+}
+
+int main(){
+
+    return 0;
+}
