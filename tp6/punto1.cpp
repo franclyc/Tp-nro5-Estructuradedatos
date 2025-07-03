7b4f550489319910e551c370abfeacf7f0817d02
franclyc <francoguanactolay61@gmail.com>
2025-07-02 23:33:23 -0300

    creando la clase nodo

---
+++ tp6/punto1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Nodo {
+public:
+    int dato;
+    Nodo* siguiente;
+    Nodo* anterior;
+
+    Nodo(int valor) {
+        dato = valor;
+        siguiente = nullptr;
+        anterior = nullptr;
+    }
+};
+
